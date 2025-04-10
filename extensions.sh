2f38ae2bf8db82c7d838897145e75f716bc1d67c
Mohit Ahirwal <167090021+mohitmobioffice@users.noreply.github.com>
2025-04-10 14:48:18 +0530

    Update extensions.sh

--- extensions.sh
+++ extensions.sh
@@ -13,5 +13,6 @@ code --install-extension wix.vscode-import-cost --force
 code --install-extension pkief.material-icon-theme --force
 code --install-extension esbenp.prettier-vscode --force
 code --install-extension ms-vscode-remote.remote-ssh --force
-code --install-extension  prisma.prisma --force
-code --install-extension  orta.vscode-jest --force 
+code --install-extension prisma.prisma --force
+code --install-extension orta.vscode-jest --force 
+code --install-extension GitHub.copilot --force
